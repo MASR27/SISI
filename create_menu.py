1efc44d4171b7c306b9121fda6ec9c4ae53b83b1
MASR27 <47757802+MASR27@users.noreply.github.com>
2023-02-14 00:30:29 -0600

    Create create_menu.py

---
+++ create_menu.py
@@ -0,0 +1,35 @@
+import os
+import json
+import markdown
+
+menu_items = {
+    "Introduction": "introduction.md",
+    "Scope": "scope.md",
+    "Normative References": "normative-references.md",
+    "Terms and Definitions": "terms-and-definitions.md",
+    "Requirements for Measurement Management Systems": "requirements-for-measurement-management-systems.md",
+    "Management Responsibility": "management-responsibility.md",
+    "Measurement System Analysis": "measurement-system-analysis.md",
+    "Measurement Equipment Control": "measurement-equipment-control.md",
+    "Measurement Process Control": "measurement-process-control.md",
+    "Data Analysis": "data-analysis.md",
+    "Improvement": "improvement.md"
+}
+
+def generate_menu():
+    menu = []
+    for item, filename in menu_items.items():
+        path = os.path.join(os.getcwd(), filename)
+        with open(path, 'r') as f:
+            content = f.read()
+            html = markdown.markdown(content)
+            menu.append(f'<li><a href="{filename}">{item}</a></li>\n')
+    return ''.join(menu)
+
+  def generate_index():
+    menu = generate_menu()
+    content = f'# ISO 10012:2003 in Gas Station\n\n<ul>\n{menu}</ul>'
+    with open('index.md', 'w') as f:
+        f.write(content)
+
+        generate_index()
